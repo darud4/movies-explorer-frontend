93966c8eb71d4af2f6b3a815f660e696159dd86b
Pavel Bezrukov <darud4@yandex.ru>
2022-06-23 23:07:32 +0400

    feat: start making promo in landing

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import Promo from './Promo/Promo';
+import './Main.css';
+
+function Main() {
+    return <section className="main">
+        <Promo />
+    </section>
+
+}
+
+
+export default Main;
