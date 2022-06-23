2ca45e31a3d4469006e25b904903ab5c5a592e1f
Pavel Bezrukov <darud4@yandex.ru>
2022-06-23 23:14:15 +0400

    feat: finish promo in desktop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,7 @@ import Main from '../Main/Main';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page">
       <Main />
     </div>
   );
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,10 +1,11 @@
 import "./Promo.css";
+import image from '../../../images/promo.svg';
 
 function Promo() {
-    return <div className="promo">
+    return (<div className="promo">
         <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-        <img className="promo__image" alt="Узор" src="../../../images/promo.svg" />
-    </div>
+        <img className="promo__image" alt="Узор" src={image} />
+    </div>)
 }
 
 export default Promo;
