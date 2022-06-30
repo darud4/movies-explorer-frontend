87605e026a5b5b23f2db43367ba680a95f55f526
Pavel Bezrukov <darud4@yandex.ru>
2022-06-30 22:50:59 +0400

    feat: make up Login margins

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,11 +8,15 @@ function Login() {
     return <form className="login" name="login">
         <Logo />
         <h1 className="title">Добро пожаловать!</h1>
-        <Input label="Имя" />
-        <Input label="E-mail" />
-        <Input label="Пароль" />
-        <Button caption="Зарегистрироваться" />
-        <span className="login__text">Уже зарегистрированы?<a href="/signin" className="login__link">Войти</a></span>
+        <fieldset className="login__inputs">
+            <Input label="Имя" />
+            <Input label="E-mail" />
+            <Input label="Пароль" />
+        </fieldset>
+        <fieldset className="login__buttons">
+            <Button caption="Зарегистрироваться" />
+            <span className="login__text">Уже зарегистрированы?<a href="/signin" className="login__link">Войти</a></span>
+        </fieldset>
     </form>
 }
 
