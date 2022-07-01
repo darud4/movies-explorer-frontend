a873d1d6ea6831e4ca003de423bf8e59ca54d26b
Pavel Bezrukov <darud4@yandex.ru>
2022-07-01 23:11:37 +0400

    feat: make Movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,6 +1,9 @@
 import './Movies.css'
+import SearchForm from './SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MovieCardList';
 
 function Movies() {
+    return <><SearchForm /><MoviesCardList /></>
 
 }
 
