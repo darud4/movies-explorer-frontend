b1565fe4d45ddc2dc5171d7c5fb79e9b91a4fc6d
Pavel Bezrukov <darud4@yandex.ru>
2022-07-02 13:18:38 +0400

    feat: make footer links actual links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
     return <footer className="footer">
         <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
         <ul className="footer__links">
-            <li className="footer__link">© 2020</li>
-            <li className="footer__link">Яндекс.Практикум</li>
-            <li className="footer__link">Github</li>
-            <li className="footer__link">Facebook</li>
+            <li className="footer__link-wrap"><span className="footer__link">© 2020</span></li>
+            <li className="footer__link-wrap"><a href="#" className="footer__link">Яндекс.Практикум</a></li>
+            <li className="footer__link-wrap"><a href="#" className="footer__link">Github</a></li>
+            <li className="footer__link-wrap"><a href="#" className="footer__link">Facebook</a></li>
         </ul>
     </footer>
 }
