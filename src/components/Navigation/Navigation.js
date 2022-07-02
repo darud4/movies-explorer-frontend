ce7d7a1e282419c3023bb08e49d4cdb2f50f6cf0
Pavel Bezrukov <darud4@yandex.ru>
2022-07-02 15:22:16 +0400

    fix: fix typo in component declaration

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import './Navigation.css';
 import { Link } from 'react-router-dom';
 
-function Navigation(isLogged) {
+function Navigation({isLogged}) {
 
     const body = !isLogged ? <nav className="navigation">        <Link className='navigation__link' to="/signup">Регистрация</Link>        <Link className='navigation__link navigation__link_green' to="/signin">Войти</Link>    </nav> :
         <nav className="navigation navigation_logged">            <Link className='navigation__link' to="/movies">Фильмы</Link>            <Link className='navigation__link' to="/saved-movies">Сохраненные фильмы</Link>            <Link className='navigation__link navigation__link_profile' to="/profile">Аккаунт</Link>        </nav>;
