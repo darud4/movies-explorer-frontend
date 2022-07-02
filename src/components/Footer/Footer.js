c99a5a083576cbb47c87b609268c82c219b48546
Pavel Bezrukov <darud4@yandex.ru>
2022-07-02 15:17:36 +0400

    fix: change 2020 in Footer from link to text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return <footer className="footer">
         <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
         <ul className="footer__links">
-            <li className="footer__link-wrap"><span className="footer__link">© 2020</span></li>
+            <li className="footer__link-wrap">© 2020</li>
             <li className="footer__link-wrap"><a href="https://practicum.yandex.ru" className="footer__link">Яндекс.Практикум</a></li>
             <li className="footer__link-wrap"><a href="https://github.com" className="footer__link">Github</a></li>
             <li className="footer__link-wrap"><a href="https://facebook.com" className="footer__link">Facebook</a></li>
