affb1e7a4bbc7949271ecf7a0c1d1bbf8c9371c5
Pavel Bezrukov <darud4@yandex.ru>
2022-07-03 10:46:26 +0400

    feat: adapt Footer for 320+

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return <footer className="footer">
         <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
         <ul className="footer__links">
-            <li className="footer__link-wrap">© 2020</li>
+            <li className="footer__link-wrap footer__link-wrap_copyright">© 2020</li>
             <li className="footer__link-wrap"><a href="https://practicum.yandex.ru" className="footer__link">Яндекс.Практикум</a></li>
             <li className="footer__link-wrap"><a href="https://github.com" className="footer__link">Github</a></li>
             <li className="footer__link-wrap"><a href="https://facebook.com" className="footer__link">Facebook</a></li>
