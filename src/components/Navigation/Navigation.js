2a147805416b1abb7e4e93f501f4982a0575fc53
Pavel Bezrukov <darud4@yandex.ru>
2022-07-03 15:36:53 +0400

    feat: make Navigation transform on 768-

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,20 @@
 import './Navigation.css';
 import { Link } from 'react-router-dom';
 
-function Navigation({isLogged}) {
+function Navigation({ isLogged }) {
 
-    const body = !isLogged ? <nav className="navigation">        <Link className='navigation__link' to="/signup">Регистрация</Link>        <Link className='navigation__link navigation__link_green' to="/signin">Войти</Link>    </nav> :
-        <nav className="navigation navigation_logged">            <Link className='navigation__link' to="/movies">Фильмы</Link>            <Link className='navigation__link' to="/saved-movies">Сохраненные фильмы</Link>            <Link className='navigation__link navigation__link_profile' to="/profile">Аккаунт</Link>        </nav>;
+    const body = !isLogged
+        ? <nav className="navigation">
+            <Link className='navigation__link' to="/signup">Регистрация</Link>
+            <Link className='navigation__link navigation__link_green' to="/signin">Войти</Link>
+        </nav>
+        :
+        <nav className="navigation navigation_logged">
+            <Link className='navigation__link navigation__link_logged' to="/movies">Фильмы</Link>
+            <Link className='navigation__link navigation__link_logged' to="/saved-movies">Сохраненные фильмы</Link>
+            <Link className='navigation__link navigation__link_logged navigation__link_profile' to="/profile">Аккаунт</Link>
+            <button className="navigation__menu" type="button"></button>
+        </nav>;
 
     return body;
 }
