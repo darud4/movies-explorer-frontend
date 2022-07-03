fa17f8047f1b2d80df9e14c7c7a6cba3041ff3c1
Pavel Bezrukov <darud4@yandex.ru>
2022-07-03 16:35:40 +0400

    feat: change links in Navtab to NavLinks

--- src/components/Navigation/NavTab/NavTab.js
+++ src/components/Navigation/NavTab/NavTab.js
@@ -1,14 +1,19 @@
 import './NavTab.css';
+import { NavLink } from 'react-router-dom';
 
 function NavTab({ isOpen = false, onClose }) {
+
+    function makeLinkActive({isActive}) {
+        return isActive ? 'navtab__link navtab__link_active' : 'navtab__link';
+    }
 
     return (isOpen &&
         <div className="navtab">
             <ul className="navtab__container">
-                <li className="navtab__item">Главная</li>
-                <li className="navtab__item">Фильмы</li>
-                <li className="navtab__item">Сохранённые фильмы</li>
-                <li className="navtab__item navtab__item_profile">Аккаунт</li>
+                <li className="navtab__item"><NavLink onClick={onClose} className={makeLinkActive} to="/">Главная</NavLink></li>
+                <li className="navtab__item"><NavLink onClick={onClose} className={makeLinkActive} to="/movies">Фильмы</NavLink></li>
+                <li className="navtab__item"><NavLink onClick={onClose} className={makeLinkActive} to="/saved-movies">Сохранённые фильмы</NavLink></li>
+                <li className="navtab__item navtab__item_profile"><NavLink className={makeLinkActive} to="/profile">Аккаунт</NavLink></li>
                 <button className="navtab__close" onClick={onClose}></button>
             </ul>
         </div>)
