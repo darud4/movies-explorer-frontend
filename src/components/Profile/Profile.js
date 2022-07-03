8d34fd21994877d3d8eaaa9db0e1dd6f2406055c
Pavel Bezrukov <darud4@yandex.ru>
2022-07-03 13:57:18 +0400

    feat: make Profile input separate component, add focus events

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,18 +1,27 @@
 import './Profile.css';
 import '../Title/Title.css';
+import ProfileInput from './ProfileInput/ProfileInput';
+import { useState } from 'react';
 
 function Profile() {
-    return <form className="profile" name='profile'>
+
+    const [name, setName] = useState('Павел');
+    const [email, setEmail] = useState('dr@dr.ru');
+
+    const handleNameChange = (newVal) => setName(newVal);
+    const handleEmailChange = (newVal) => setEmail(newVal);
+
+    return (<form className="profile" name='profile'>
         <h2 className="title">Привет, Виталий!</h2>
         <fieldset className="profile__inputs">
-            <label className="profile__label">Имя <input name="name" className="profile__input" value="Виталий" /> </label>
-            <label className="profile__label">E-mail <input name="name" className="profile__input" value="pochta@yandex.ru" /> </label>
+            <ProfileInput caption="Имя" name='name' placeholder='Введите имя' value={name} onChange={handleNameChange} />
+            <ProfileInput caption="E-mail" name='email' placeholder='Введите адрес электронной почты' value={email} onChange={handleEmailChange} />
         </fieldset>
         <fieldset className="profile__buttons">
             <button type="button" className="profile__button">Редактировать</button>
             <button type="button" className="profile__button profile__button_attention">Выйти из аккаунта</button>
         </fieldset>
-    </form>
+    </form>);
 }
 
 export default Profile;
