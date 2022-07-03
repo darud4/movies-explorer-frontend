1eb0d92d26587f1a57440c72381e30e292e85119
Pavel Bezrukov <darud4@yandex.ru>
2022-07-03 17:49:26 +0400

    fix: fix typo

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,7 +4,7 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 
 function MoviesCardList({ movies = [] }) {
     return <ul className="movies-card-list">
-        {movies.length === 0 ? <li movies-card-list__title>Фильмов не найдено</li>
+        {movies.length === 0 ? <li className="movies-card-list__title">Фильмов не найдено</li>
             : movies.map(({ image, name, duration }, i) => <MoviesCard key={i} image={image} name={name} duration={duration} />)
         }
     </ul>
