c8ec8547516a2e0c370d0e1f6c5e758bff9ae364
Pavel Bezrukov <darud4@yandex.ru>
2022-07-10 10:27:10 +0400

    fix: undef pattern in input makes input always invalid

--- src/components/InputForm/Input/Input.js
+++ src/components/InputForm/Input/Input.js
@@ -2,7 +2,7 @@ import './Input.css';
 
 function Input({ label, name, error, onChange, value, type = 'text', pattern }) {
     return <label className="input__label">{label}
-        <input className="input__input" name={name} required value={value || ''} onChange={onChange} type={type} pattern={pattern} />
+        <input className="input__input" name={name} required value={value || ''} onChange={onChange} type={type} pattern={pattern || false} />
         <span className={`input__error-span error-${name}`}>{error}</span>
     </label>
 }
