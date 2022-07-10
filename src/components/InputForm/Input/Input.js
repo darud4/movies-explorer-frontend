504974556582a10df08c60852d25886786bc15fc
Pavel Bezrukov <darud4@yandex.ru>
2022-07-10 10:30:57 +0400

    refactor (Input): if pattern is undef, omit it

--- src/components/InputForm/Input/Input.js
+++ src/components/InputForm/Input/Input.js
@@ -1,8 +1,8 @@
 import './Input.css';
 
-function Input({ label, name, error, onChange, value, type = 'text', pattern }) {
+function Input({ label, name, error, onChange, value = '', type = 'text', pattern }) {
     return <label className="input__label">{label}
-        <input className="input__input" name={name} required value={value || ''} onChange={onChange} type={type} pattern={pattern || false} />
+        <input className="input__input" name={name} required value={value} onChange={onChange} type={type} pattern={pattern || undefined} />
         <span className={`input__error-span error-${name}`}>{error}</span>
     </label>
 }
